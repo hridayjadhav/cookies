160ae9b8231554b6e13e2cb21c45547a5b10a7d9
Hriday <hridayjadhav789@gmail.com>
2023-09-11 11:47:06 +0530

    here we have done some excercise with the help of cookies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'data-storing';
+
+  constructor(private cookie : CookieService){ }    //in this we have injected the cookieService.
+
+  setCookie(){      // Here we have se the 2 cookies, to see the delete and delete all funtion.
+    this.cookie.set("userId", "adminUser");
+    this.cookie.set("userName", "Hriday Jadhav");
+  }
+
+  getCookie(){   //Here we have get the value on the console. ( Hriday Jadhav )
+    console.log(this.cookie.get("userName")  );
+    
+  }
+
+  deleteCookie(){    //from this we will delete perticular data / value
+    console.log(this.cookie.delete("userName")  );
+    
+  }
+
+  deleteAllCookie(){    // here we can delete all the cookies .
+    this.cookie.deleteAll();
+    
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CookieService } from 'ngx-cookie-service'
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
